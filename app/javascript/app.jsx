1056b5a22a66852335d84952a3ee50b47b72472e
Joshua Flanagan <joshuaflanagan@gmail.com>
2017-12-14 18:55:05 -0600

    Demonstrate pulling in a stylesheet

--- app/javascript/app.jsx
+++ app/javascript/app.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "./styles.css"
 
 function renderApplication(hostOptions) {
   console.log("running renderApplication with:", hostOptions)
@@ -10,7 +11,7 @@ function renderApplication(hostOptions) {
     (
       <div>
         <span>This is react</span>
-        <img src={require("./yankees_ny")}/>
+        <img src={require("./products.png")}/>
       </div>
     ),
     mountNode
